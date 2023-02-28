146442b99f24d8a654c689c51877f5c45aba9f1e
JESurzyn <jordan.surzyn@gmail.com>
2023-02-28 08:26:11 -0500

    last five results code plan

---
+++ lastfive.js
@@ -0,0 +1,14 @@
+// project plan 
+// - create a function that runs on button click 
+//     - actually probably only want it to run if the api returns anything
+// - function will
+//     - take input from search bar 
+//     - set the input to local Storage
+//         localStorage.setItem(loc1, input)
+//     - there's a loop -> or an if
+//         - if less 5 in local storage keep adding
+//         - if 5 or more, kick out the last entry
+
+// - create another function or code that reads what's in local storage 
+//     - display the results as buttons or hrefs and (maybe with the icon as well and the date)
+//     - make the buttons clickable to make a request from api
